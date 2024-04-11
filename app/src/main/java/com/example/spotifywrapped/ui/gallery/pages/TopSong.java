9b5aeb2de45d5f2d340b4d912d9e377db6687252
Noah G <ngreene2026@gmail.com>
2024-04-11 08:07:14 -0400

    working thru slideshow 1

--- app/src/main/java/com/example/spotifywrapped/ui/gallery/pages/TopSong.java
+++ app/src/main/java/com/example/spotifywrapped/ui/gallery/pages/TopSong.java
@@ -2,65 +2,47 @@ package com.example.spotifywrapped.ui.gallery.pages;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.example.spotifywrapped.R;
+import com.example.spotifywrapped.databinding.FragmentTopSongBinding;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TopSong#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class TopSong extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TopSong() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TopSong.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TopSong newInstance(String param1, String param2) {
-        TopSong fragment = new TopSong();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentTopSongBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_song, container, false);
+        binding = FragmentTopSongBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        // Set the click listener for the button
+        binding.topsongnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(v);
+                navController.navigate(R.id.nav_topArtist);
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
