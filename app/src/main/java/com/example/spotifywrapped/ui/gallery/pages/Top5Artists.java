237ed427cbc8b99e2039ba076cf693e6bdd70dc3
KaiHackney <kaiahackney@gmail.com>
2024-04-11 13:18:56 -0400

    wrap done

--- app/src/main/java/com/example/spotifywrapped/ui/gallery/pages/Top5Artists.java
+++ app/src/main/java/com/example/spotifywrapped/ui/gallery/pages/Top5Artists.java
@@ -1,5 +1,9 @@
 package com.example.spotifywrapped.ui.gallery.pages;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,25 +11,95 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.bumptech.glide.Glide;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.spotifywrapped.MainActivity;
 import com.example.spotifywrapped.R;
 import com.example.spotifywrapped.databinding.FragmentTop5ArtistsBinding;
+import com.example.spotifywrapped.user.User;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Top5Artists extends Fragment {
+    private User loadUser() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String userJson = sharedPreferences.getString("CurrentUser", null);
+        Log.d("SharedPreferences", "Loaded token: " + userJson);
+        return gson.fromJson(userJson, User.class);
+    }
 
     private FragmentTop5ArtistsBinding binding;
+
+    public static Context context;
+
+    private static User currentUser;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentTop5ArtistsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        context = MainActivity.getInstance();
+        currentUser = loadUser();
+
+        ArrayList<Map<String, Object>> wraps = currentUser.getwraps();
+        if (!wraps.isEmpty()) {
+            Map<String, Object> wrap = wraps.get(0);
+            String name1 = (String) ((ArrayList<String>) wrap.get("artists")).get(0);
+            String image1 = (String) ((ArrayList<String>) wrap.get("artistsimage")).get(0);
+            String name2 = (String) ((ArrayList<String>) wrap.get("artists")).get(1);
+            String image2 = (String) ((ArrayList<String>) wrap.get("artistsimage")).get(1);
+            String name3 = (String) ((ArrayList<String>) wrap.get("artists")).get(2);
+            String image3 = (String) ((ArrayList<String>) wrap.get("artistsimage")).get(2);
+            String name4 = (String) ((ArrayList<String>) wrap.get("artists")).get(3);
+            String image4 = (String) ((ArrayList<String>) wrap.get("artistsimage")).get(3);
+            String name5 = (String) ((ArrayList<String>) wrap.get("artists")).get(4);
+            String image5 = (String) ((ArrayList<String>) wrap.get("artistsimage")).get(4);
+            TextView songName = (TextView) root.findViewById(R.id.topartisttext1);
+            songName.setText(name1);
+            songName = (TextView) root.findViewById(R.id.topartisttext2);
+            songName.setText(name2);
+            songName = (TextView) root.findViewById(R.id.topartisttext3);
+            songName.setText(name3);
+            songName = (TextView) root.findViewById(R.id.topartisttext4);
+            songName.setText(name4);
+            songName = (TextView) root.findViewById(R.id.topartisttext5);
+            songName.setText(name5);
+            ImageView topsongimage = (ImageView) root.findViewById(R.id.topartistimage1);
+            Glide.with(context)
+                    .load(image1)
+                    .into(topsongimage);
+            topsongimage = (ImageView) root.findViewById(R.id.topartistimage2);
+            Glide.with(context)
+                    .load(image2)
+                    .into(topsongimage);
+            topsongimage = (ImageView) root.findViewById(R.id.topartistimage3);
+            Glide.with(context)
+                    .load(image3)
+                    .into(topsongimage);
+            topsongimage = (ImageView) root.findViewById(R.id.topartistimage4);
+            Glide.with(context)
+                    .load(image4)
+                    .into(topsongimage);
+            topsongimage = (ImageView) root.findViewById(R.id.topartistimage5);
+            Glide.with(context)
+                    .load(image5)
+                    .into(topsongimage);
+        }
+
 
         // Set the click listener for the button
         binding.top5artistsnext.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/spotifywrapped/ui/gallery/pages/TopGenre.java
+++ app/src/main/java/com/example/spotifywrapped/ui/gallery/pages/TopGenre.java
@@ -1,5 +1,9 @@
 package com.example.spotifywrapped.ui.gallery.pages;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,13 +11,24 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.bumptech.glide.Glide;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.spotifywrapped.MainActivity;
 import com.example.spotifywrapped.R;
 import com.example.spotifywrapped.databinding.FragmentTop5ArtistsBinding;
 import com.example.spotifywrapped.databinding.FragmentTopGenreBinding;
+import com.example.spotifywrapped.user.User;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,12 +36,39 @@ import com.example.spotifywrapped.databinding.FragmentTopGenreBinding;
 public class TopGenre extends Fragment {
 
     private FragmentTopGenreBinding binding;
+    public static Context context;
+
+    private static User currentUser;
+
+    private User loadUser() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String userJson = sharedPreferences.getString("CurrentUser", null);
+        Log.d("SharedPreferences", "Loaded token: " + userJson);
+        return gson.fromJson(userJson, User.class);
+    }
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentTopGenreBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        context = MainActivity.getInstance();
+        currentUser = loadUser();
+
+        ArrayList<Map<String, Object>> wraps = currentUser.getwraps();
+        if (!wraps.isEmpty()) {
+            Map<String, Object> wrap = wraps.get(0);
+            String genre = (String) ((ArrayList<String>) wrap.get("artistsgenre")).get(0);
+            String image = (String) ((ArrayList<String>) wrap.get("artistsimage")).get(0);
+            TextView artistName = (TextView) root.findViewById(R.id.topgenre);
+            artistName.setText(genre);
+            ImageView topartistimage = (ImageView) root.findViewById(R.id.genreimage);
+            Glide.with(context)
+                    .load(image)
+                    .into(topartistimage);
+        }
 
         // Set the click listener for the button
         binding.topgenrenext.setOnClickListener(new View.OnClickListener() {
