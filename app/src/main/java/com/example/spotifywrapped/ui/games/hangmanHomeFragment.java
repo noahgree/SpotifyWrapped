8889b3b03b0da100a33c4662d46a5e03e3acbd09
KaiHackney <kaiahackney@gmail.com>
2024-04-16 17:00:59 -0400

    api call

--- app/src/main/java/com/example/spotifywrapped/ui/games/hangmanHomeFragment.java
+++ app/src/main/java/com/example/spotifywrapped/ui/games/hangmanHomeFragment.java
@@ -21,6 +21,8 @@ import androidx.navigation.Navigation;
 
 import com.example.spotifywrapped.MainActivity;
 import com.example.spotifywrapped.R;
+import com.example.spotifywrapped.databinding.FragmentHangmanHomeBinding;
+import com.example.spotifywrapped.databinding.FragmentHomeBinding;
 import com.example.spotifywrapped.databinding.FragmentMatchingHomeBinding;
 import com.example.spotifywrapped.ui.gallery.AddWrapFragment;
 import com.example.spotifywrapped.user.User;
@@ -49,6 +51,127 @@ import okhttp3.Response;
  * create an instance of this fragment.
  */
 public class hangmanHomeFragment extends Fragment {
+
+    private FragmentHangmanHomeBinding binding;
+
+
+    public static Context context;
+    private static User currentUser;
+    private static final OkHttpClient mOkHttpClient = new OkHttpClient();
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = MainActivity.getInstance();
+        currentUser = loadUser();
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+    }
+
+    private User loadUser() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String userJson = sharedPreferences.getString("CurrentUser", null);
+        Log.d("SharedPreferences", "Loaded token: " + userJson);
+        return gson.fromJson(userJson, User.class);
+    }
+
+
+    public static String getSpotifyToken() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        String string = sharedPreferences.getString("SpotifyToken", null);
+        Log.d("SharedPreferences", "Loaded token: " + string);
+        return sharedPreferences.getString("SpotifyToken", null);
+    }
+
+    public static void onMatchStarted(Context context, View view, OkHttpClient okHttpClient, String term, Map<String, Object> wrap, AddWrapFragment.DataCompletionHandler handler) {
+        if (getSpotifyToken() == null) {
+            Toast.makeText(context, "You need to get an access token first!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me/top/artists?time_range=" + term + "_term&limit=10")
+                .addHeader("Authorization", "Bearer " + getSpotifyToken())
+                .build();
+
+        Call mCall = okHttpClient.newCall(request);
+
+        mCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("HTTP", "Failed to fetch data: " + e);
+                new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(context, "Failed to fetch data, watch Logcat for more details", Toast.LENGTH_SHORT).show());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseData = response.body().string();
+                try {
+                    final JSONObject jsonObject = new JSONObject(responseData);
+                    new Handler(Looper.getMainLooper()).post(() -> {
+                        try {
+                            if (jsonObject.getJSONArray("items").length() > 0) {
+                                List<String> name = new ArrayList<>();
+                                for (int i = 0; i < 8; i++) {
+                                    name.add(jsonObject.getJSONArray("items").getJSONObject(i).getString("name"));
+                                }
+                                wrap.put("artists", name);
+
+                                if (handler != null) {
+                                    handler.onDataCompleted(wrap);
+                                }
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                } catch (JSONException e) {
+                    Log.d("JSON", "Failed to parse data: " + e);
+                    new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(context, "Failed to parse data, watch Logcat for more details", Toast.LENGTH_SHORT).show());
+                }
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentHangmanHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        Button playButton = (Button) root.findViewById(R.id.hangmanstart);
+        Spinner spinner = null;//(Spinner) root.findViewById(R.id.timeFrameSpinner);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String term = "long";//spinner.getSelectedItem().toString().split(" ")[0].toLowerCase();
+                Map<String, Object> wrap = new HashMap<>();
+
+
+                AddWrapFragment.DataCompletionHandler handler = updatedImages -> {
+                    // This block will be called once data fetching is complete.
+                    getActivity().runOnUiThread(() -> {
+                        // switch to matching game view
+                        List<String> images = (List<String>) updatedImages.get("artists");
+                        NavController navController = Navigation.findNavController(v);
+                        //navController.navigate(R.id.hangmanGame);
+                    });
+                };
+
+
+                onMatchStarted(context, root, mOkHttpClient, term, wrap, handler);
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return root;
+
+    }
 
 
 }
